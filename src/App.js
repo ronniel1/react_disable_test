14bbe6ed895fe7b023cd1dce7f59eef789ab9800
Ronnie Lazar <ronnie@sptl.me>
2021-09-14 18:05:31 +0300

    Example showing where IconButton does not play nice with the fieldset 'disabled' property. In this example the IconButton is passed a 'disabled' property expicitly. Without this, it will not be disabled when the fieldset is disabled

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { useToggle } from "react-use";
+import { IconButton, TextField, Collapse } from "@material-ui/core";
+import { ExpandMore } from "@material-ui/icons";
+import "./App.css";
 
 function App() {
+  const [isDisabled, toggleIsDisabled] = useToggle(false);
+  const [isOpen, toggleIsOpen] = useToggle(false);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={toggleIsDisabled}>
+        {isDisabled ? "enable" : "disable"}
+      </button>
+      <fieldset disabled={isDisabled}>
+        <label>{isDisabled ? "fields disabled" : "Fields enabled"}</label>
+        <TextField label="field1" />
+        <div>
+          <IconButton disabled={isDisabled} onClick={toggleIsOpen}>
+            <ExpandMore className={isOpen ? "rotate" : ""} />
+          </IconButton>
+          <Collapse in={isOpen}>
+            <p>111</p>
+            <p>222</p>
+          </Collapse>
+        </div>
+      </fieldset>
     </div>
   );
 }
